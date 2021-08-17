98414f6b3e16dcc8a76dbf662797677618513398
engureguo <8440193+engureguo@user.noreply.gitee.com>
2021-08-17 15:09:48 +0800

    秒杀显示

--- src/main/java/com/engure/seckill/controller/GoodsController.java
+++ src/main/java/com/engure/seckill/controller/GoodsController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -53,8 +54,30 @@ public class GoodsController {
     public String detail(@PathVariable Long goodsId, User user, Model model) {
         if (null == user) return "login";
 
+        GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
         model.addAttribute("user", user);
-        model.addAttribute("goods", goodsService.findGoodsVoByGoodsId(goodsId));
+        model.addAttribute("goods", goodsVo);
+
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+        //秒杀状态 0未开始，1进行中，2已结束
+        int secKillStatus = 0;
+        //秒杀倒计时 >0倒计时，0进行中，-1已结束
+        int remainSeconds = 0;
+        //秒杀还未开始
+        if (nowDate.before(startDate)) {
+            remainSeconds = ((int) ((startDate.getTime() - nowDate.getTime()) / 1000));
+        } else if (nowDate.after(endDate)) {
+            //	秒杀已结束
+            secKillStatus = 2;
+            remainSeconds = -1;
+        } else {
+            //秒杀中
+            secKillStatus = 1;
+        }
+        model.addAttribute("remainSeconds", remainSeconds);
+        model.addAttribute("secKillStatus", secKillStatus);
 
         return "goodsDetail";
     }
